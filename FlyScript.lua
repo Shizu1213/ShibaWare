d9b676a8e024cd389b5d17d1d88583410b196c6f
Bloo <52343985+Shizu1213@users.noreply.github.com>
2023-04-15 21:33:20 -0400

    Update FlyScript.lua

--- FlyScript.lua
+++ FlyScript.lua
@@ -1,39 +1,30 @@
-local player = game.Players.LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-
--- Listen for PlayerGui being added
-playerGui.ChildAdded:Connect(function(child)
-    if child.Name == "MyScreenGui" then
-        child.Enabled = true
-    end
-end)
-
--- Create and add the ScreenGui to PlayerGui
-local myGui = Instance.new("ScreenGui")
-myGui.Name = "MyScreenGui"
-myGui.Parent = playerGui
-myGui.Enabled = false
-
-local plr = game.Players.LocalPlayer
-local char = plr.Character or plr.CharacterAdded:Wait()
-
+-- Variables
 local flying = false
-
+local player = game.Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
 local torso = char:WaitForChild("HumanoidRootPart")
 local bodyvel = Instance.new("BodyVelocity", torso)
 bodyvel.Name = "flyvel"
 bodyvel.Velocity = Vector3.new(0,0,0)
 bodyvel.MaxForce = Vector3.new(0,math.huge,0)
 
-game:GetService("UserInputService").InputBegan:Connect(function(input, isTyping)
-    if isTyping then return end
-    if input.KeyCode == Enum.KeyCode.W then
-        bodyvel.Velocity = Vector3.new(0,50,0)
-        flying = true
-    elseif input.KeyCode == Enum.KeyCode.S then
-        bodyvel.Velocity = Vector3.new(0,0,0)
-        flying = false
-    end
-end)
+-- Function to fly
+local function fly()
+    local inputService = game:GetService("UserInputService")
+    inputService.InputBegan:Connect(function(input, isTyping)
+        if isTyping then return end
+        if input.KeyCode == Enum.KeyCode.W then
+            bodyvel.Velocity = Vector3.new(0,50,0)
+            flying = true
+        elseif input.KeyCode == Enum.KeyCode.S then
+            bodyvel.Velocity = Vector3.new(0,0,0)
+            flying = false
+        end
+    end)
+end
 
-return { flying = flying }
+-- Return values
+return {
+    flying = flying,
+    fly = fly,
+}
