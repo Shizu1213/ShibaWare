33a6e862fa3fe5695bef291a734987c1cb0c7739
Bloo <52343985+Shizu1213@users.noreply.github.com>
2023-04-15 22:11:36 -0400

    Update FlyScript.lua

--- FlyScript.lua
+++ FlyScript.lua
@@ -1,14 +1,25 @@
 local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
 
-local flying = false
+local speed = 50
 
-local bodyVelocity = char.HumanoidRootPart:FindFirstChild("flyvel")
-if not bodyVelocity then
-    bodyVelocity = Instance.new("BodyVelocity", char.HumanoidRootPart)
-    bodyVelocity.Name = "flyvel"
-    bodyVelocity.Velocity = Vector3.new(0, 0, 0)
-    bodyVelocity.MaxForce = Vector3.new(0, math.huge, 0)
+local bodyVelocity = Instance.new("BodyVelocity", char.HumanoidRootPart)
+bodyVelocity.Name = "flyvel"
+bodyVelocity.Velocity = Vector3.new(0, 0, 0)
+bodyVelocity.MaxForce = Vector3.new(0, math.huge, 0)
+
+local function updateVelocity()
+    local verticalSpeed = 0
+    
+    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Space) then
+        verticalSpeed = speed
+    elseif game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then
+        verticalSpeed = -speed
+    end
+    
+    bodyVelocity.Velocity = Vector3.new(0, verticalSpeed, 0)
 end
 
-return {flying = flying}
+game:GetService("RunService").RenderStepped:Connect(updateVelocity)
+
+return {flying = false} -- Disable flying by default
