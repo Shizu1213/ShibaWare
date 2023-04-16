1c20fda522741a4de9c97a1b11ce6c6f4a38b48e
Bloo <52343985+Shizu1213@users.noreply.github.com>
2023-04-15 21:44:54 -0400

    you should now fly up and down

--- FlyScript.lua
+++ FlyScript.lua
@@ -1,30 +1,26 @@
--- Variables
-local flying = false
 local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
-local torso = char:WaitForChild("HumanoidRootPart")
-local bodyvel = Instance.new("BodyVelocity", torso)
-bodyvel.Name = "flyvel"
-bodyvel.Velocity = Vector3.new(0,0,0)
-bodyvel.MaxForce = Vector3.new(0,math.huge,0)
 
--- Function to fly
-local function fly()
-    local inputService = game:GetService("UserInputService")
-    inputService.InputBegan:Connect(function(input, isTyping)
-        if isTyping then return end
-        if input.KeyCode == Enum.KeyCode.W then
-            bodyvel.Velocity = Vector3.new(0,50,0)
-            flying = true
-        elseif input.KeyCode == Enum.KeyCode.S then
-            bodyvel.Velocity = Vector3.new(0,0,0)
-            flying = false
-        end
-    end)
+local flying = false
+local speed = 50
+
+local bodyVelocity = Instance.new("BodyVelocity", char.HumanoidRootPart)
+bodyVelocity.Name = "flyvel"
+bodyVelocity.Velocity = Vector3.new(0, 0, 0)
+bodyVelocity.MaxForce = Vector3.new(0, math.huge, 0)
+
+local function updateVelocity()
+    local verticalSpeed = 0
+    
+    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Space) then
+        verticalSpeed = speed
+    elseif game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then
+        verticalSpeed = -speed
+    end
+    
+    bodyVelocity.Velocity = Vector3.new(0, verticalSpeed, 0)
 end
 
--- Return values
-return {
-    flying = flying,
-    fly = fly,
-}
+game:GetService("RunService").RenderStepped:Connect(updateVelocity)
+
+return {flying = flying}
