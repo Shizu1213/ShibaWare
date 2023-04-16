d151af2a695e3ad6443cefa72aaa534b704730b8
Bloo <52343985+Shizu1213@users.noreply.github.com>
2023-04-15 20:50:57 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,28 @@
+local flyScriptURL = https://github.com/Shizu1213/ShibaWare/blob/main/FlyScript.lua
+
+local function toggleFly()
+    local flying = require(game:GetService("ReplicatedStorage").FlyScript).flying
+    require(game:GetService("ReplicatedStorage").FlyScript).flying = not flying
+end
+
+local gui = Instance.new("ScreenGui")
+gui.Enabled = false -- Hide the GUI initially
+gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local flyButton = Instance.new("TextButton")
+flyButton.Size = UDim2.new(0, 100, 0, 50)
+flyButton.Position = UDim2.new(0.5, -50, 0.5, -25)
+flyButton.Text = "Fly"
+flyButton.Parent = gui
+
+local function toggleGUI(input, gameProcessedEvent)
+    if input.KeyCode == Enum.KeyCode.RightShift then
+        gui.Enabled = not gui.Enabled
+    end
+end
+
+loadstring(game:HttpGet(flyScriptURL))()
+
+flyButton.MouseButton1Click:Connect(toggleFly)
+
+game:GetService("UserInputService").InputBegan:Connect(toggleGUI)
