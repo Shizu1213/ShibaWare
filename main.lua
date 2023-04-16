ea458a2fe9acee24f3788163df52c133d31342b9
Bloo <52343985+Shizu1213@users.noreply.github.com>
2023-04-15 21:37:22 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Load FlyScript module
-local flyScript = require(script.Parent.FlyScript)
+local flyScript = require(game:GetService("ReplicatedStorage").FlyScript)
 
 -- Function to toggle fly
 local function toggleFly()
@@ -18,7 +18,6 @@ end
 local gui = Instance.new("ScreenGui")
 gui.Enabled = false
 gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-print(gui.Parent)
 
 local flyButton = Instance.new("TextButton")
 flyButton.Size = UDim2.new(0, 100, 0, 50)
@@ -28,8 +27,6 @@ flyButton.Parent = gui
 
 -- Function to toggle GUI
 local function toggleGUI(input, gameProcessedEvent)
-    print("toggleGUI function called")
-    print(input.KeyCode)
     if input.KeyCode == Enum.KeyCode.RightShift then
         gui.Enabled = not gui.Enabled
     end
